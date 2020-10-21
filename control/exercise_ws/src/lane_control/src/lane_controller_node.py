1b1f7be09051d89ac2b27761376b48407318530e
Raphael Jean <raphael.jean@rocketmail.com>
2020-10-21 18:38:48 -0400

    WIP

--- control/exercise_ws/src/lane_control/src/lane_controller_node.py
+++ control/exercise_ws/src/lane_control/src/lane_controller_node.py
@@ -2,14 +2,54 @@
 import numpy as np
 import rospy
 import debugpy
+import os
+import json
 
 from duckietown.dtros import DTROS, NodeType, TopicType, DTParam, ParamType
 from duckietown_msgs.msg import Twist2DStamped, LanePose, WheelsCmdStamped, BoolStamped, FSMState, StopLineReading, SegmentList
 
 from lane_controller.controller import PurePursuitLaneController
+from scipy.optimize import curve_fit
 
 # 5678 is the default attach port in the VS Code debug configurations. Unless a host and port are specified, host defaults to 127.0.0.1
 
+def get_xy(points):
+    x = []
+    y = []
+    for point in points:
+        x.append(point[0][0])
+        x.append(point[1][0])
+        y.append(point[0][1])
+        y.append(point[1][1])
+    return (x,y)
+
+def fit_and_show(x,y, c="b"):
+    a,b = fit(x,y)
+    show_line(a,b,c)
+    return a,b
+
+
+def fit(x,y, c="b"):
+    if len(x)>3:
+        popt, pcov = curve_fit(line_func, x, y)
+        return popt
+    else:
+        raise ValueError("Not enough data points")
+
+        
+def line_func(x, a, b):
+    #return a * np.exp(-b * x) + c
+    #return a * x**2 + b*x + c
+    return a*x + b
+
+def show_line(a,b,c):
+    model_x = np.arange(0,1,0.01)
+    model_y = line_func(model_x, a,b)
+    plt.plot(model_x, model_y, c)
+
+def get_aim_point(a,b, dist, offset):
+    x, y = dist,dist*a+b-offset
+    return (x,y)
 
 
 class LaneControllerNode(DTROS):
@@ -50,8 +90,16 @@ class LaneControllerNode(DTROS):
                                                  self.cbLanePoses,
                                                  queue_size=1)
         self.breakpoints_enabled=False
-
-        self.sub_ground_projected_lanes = rospy.Subscriber("/agent/ground_projection_node/lineseglist_out",
+        # Line Segments: 
+        #line_segment_node = "/agent/ground_projection_node/lineseglist_out"
+        agent = node_name.split("/")[0]
+        print(node_name.split("/"))
+        #line_segment_node = f"/{agent}/lane_filter_node/seglist_filtered"
+        line_segment_node = "lane_filter_node/seglist_filtered"
+        self.log(f"Filtered segment topic : {line_segment_node}")
+        
+        #line_segment_node = "/agent/lane_filter_node/seglist_filtered"
+        self.sub_ground_projected_lanes = rospy.Subscriber( line_segment_node,
                                                             SegmentList,
                                                             self.cbGroundProjectedLineSegments,
                                                             queue_size=1)
@@ -60,13 +108,22 @@ class LaneControllerNode(DTROS):
         debugpy.listen(5678)
         self.log("Waiting for debugger attach")
 
-        self.right_offset = 0.12
-        self.lookup_distance = 0.25
-        self.lookup_depth = 0.05
+        self.right_offset = 0.18
+        self.lookup_distance = 0.2
+        self.lookup_depth = 0.2
+        self.white_lookup_distance=0.4
         self.max_speed = 0.1
         self.K = 1
         self.last_omega=0
         self.last_v = self.max_speed
+        self.last_datalog = None
+
+        self.last_aim_point = (0.2, 0)
+
+        rospy.set_param("relative_name", 10.0)
+
+        if not os.path.exists("/code/datalog"):
+            os.mkdir("/code/datalog")
         #debugpy.wait_for_client()
         
         #self.log('break on this line')
@@ -86,6 +143,7 @@ class LaneControllerNode(DTROS):
         car_control_msg.v = 0.5
         car_control_msg.omega = 0
 
+
         #if self.breakpoints_enabled:
         #    debugpy.breakpoint()
         #    self.log('break on this line')
@@ -103,6 +161,8 @@ class LaneControllerNode(DTROS):
         car_control_msg = Twist2DStamped()
         car_control_msg.header = self.segments_msg.header
 
+        relative_name = rospy.get_param("relative_name")
+
 
 
         if self.breakpoints_enabled:
@@ -113,6 +173,8 @@ class LaneControllerNode(DTROS):
         yellow_lines_np = []
         white_lines = []
         white_lines_np = []
+
+        lines_dict = {}
 
         for segment in segments_msg.segments:
             assert len(segment.points)==2
@@ -128,6 +190,7 @@ class LaneControllerNode(DTROS):
                                         segment.points[1].x,
                                         segment.points[1].y])
 
+
             elif segment.color==0:
                 #White line.
                 white_lines.append((start,end))
@@ -138,6 +201,16 @@ class LaneControllerNode(DTROS):
 
         yellow_lines_np = np.array(yellow_lines_np)
         white_lines_np = np.array(white_lines_np)
+
+        lines = {}
+        lines["white"] = white_lines
+        lines["yellow"] = yellow_lines
+
+        datalog = json.dumps(lines)
+        if self.last_datalog!=datalog:
+            with open(f"/code/datalog/segments_{segments_msg.header.seq}.json", "w") as f:
+                f.write(datalog)
+            self.last_datalog = datalog
 
         if self.breakpoints_enabled:
             debugpy.breakpoint()
@@ -152,20 +225,93 @@ class LaneControllerNode(DTROS):
         car_control_msg.v = self.last_v
         car_control_msg.omega = self.last_omega
 
+        found_yellow_line = False
+        found_white_right_line = False
+
         #Basic line detection        
         if len(yellow_lines_np)>0:
             dist_p1 = np.sqrt(yellow_lines_np[:,0]**2 + yellow_lines_np[:,1]**2)
             #dist_p2 = np.sqrt(yellow_lines_np[:,2]**2 + yellow_lines_np[:,3]**2)
-            valid_points = yellow_lines_np[(dist_p1 > 0.3) & (dist_p1 < 0.6)]
-            if len(valid_points)>0:
-                aim_x = valid_points[:,[0,2]].mean()
-                aim_y = valid_points[:,[1,3]].mean() - self.right_offset
-                match=True
-                car_control_msg.v = self.max_speed
-                alpha = np.arctan(aim_y/aim_x)
-                car_control_msg.omega = np.sin(alpha) / self.K
+            valid_points = yellow_lines_np[
+                (dist_p1 > self.lookup_distance-self.lookup_depth) & 
+                (dist_p1 < self.lookup_distance+self.lookup_depth)]
 
-        self.log(f"v={car_control_msg.v}, omega = {car_control_msg.omega:.2f}. Aiming at {aim_x:.2f}, {aim_y:.2f}. Match={match}")
+            #The yellow line should not be right of the duckie.
+            #valid_points = valid_points[(valid_points[:,1]>0) & (valid_points[:,3]>0)]
+
+            if len(valid_points)>0:
+                aim_x = np.median(valid_points[:,[0,2]])
+                aim_y = np.median(valid_points[:,[1,3]]) - self.right_offset
+                match=True
+                found_yellow_line = True
+        
+        if len(white_lines_np)>0:
+            dist_p1 = np.sqrt(white_lines_np[:,0]**2 + white_lines_np[:,1]**2)
+            #dist_p2 = np.sqrt(yellow_lines_np[:,2]**2 + yellow_lines_np[:,3]**2)
+            valid_points=white_lines_np
+            #valid_points = white_lines_np[
+            #    (dist_p1 > self.white_lookup_distance-self.lookup_depth) & 
+            #    (dist_p1 < self.white_lookup_distance+self.lookup_depth)]
+            right_white_line = valid_points[(valid_points[:,1]<0) & (valid_points[:,3]<0)]
+            #The right white line cannot be more than 25 centimeters ahead
+            right_white_line = valid_points[(valid_points[:,0]<0.25) & (valid_points[:,2]<0.25)]
+            
+            if len(right_white_line)>0 and not found_yellow_line:
+                #We did not see the yellow line, so 
+                #the next best guess in to follow the white right line.
+                found_white_right_line=True
+                aim_x = np.median(right_white_line[:,[0,2]])
+                aim_y = np.median(right_white_line[:,[1,3]]) + self.right_offset
+
+        match = found_white_right_line | found_yellow_line
+        if match:
+            car_control_msg.v = self.max_speed
+            alpha = np.arctan(aim_y/aim_x)
+            car_control_msg.omega = np.sin(alpha) / self.K
+
+        yellow_aim_point = None
+        white_aim_point = None
+        #New code here
+        x, y = get_xy(lines["white"])
+        try:
+            a,b = fit(x,y)
+            white_aim_point = get_aim_point(a,b,self.lookup_distance,-self.right_offset)
+        except ValueError:
+            pass
+
+        x, y = get_xy(lines["yellow"])
+        try:
+            a,b = fit(x,y)
+            yellow_aim_point = get_aim_point(a,b,self.lookup_distance,self.right_offset)
+        except ValueError:
+            pass
+
+        aim_point=None
+        if yellow_aim_point:
+            aim_point = yellow_aim_point
+            if white_aim_point:
+                aim_point = (
+                                ((yellow_aim_point[0] + white_aim_point[0]) / 2),
+                                ((yellow_aim_point[1] + white_aim_point[1]) / 2)
+                )
+        else:
+            aim_point = white_aim_point
+
+        if aim_point is None:
+            aim_point = self.last_aim_point
+        else:
+            self.last_aim_point=aim_point
+
+        car_control_msg.v = self.max_speed
+        alpha = np.arctan(aim_point[1]/aim_point[0])
+        car_control_msg.omega = np.sin(alpha) / self.K
+
+        self.log(f"v={car_control_msg.v}, omega = {car_control_msg.omega:.2f}. Aim: {aim_point[0]:2.f},{aim_point[1]:2.f. y:.2f}, {relative_name}")
+
+        #self.log(f"Aim point:"{aim_point})
+
+        self.last_omega = car_control_msg.omega
+        self.last_v = car_control_msg.v
 
         self.publishCmd(car_control_msg)
 
