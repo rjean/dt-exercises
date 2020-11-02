25071dda1337f85b5af45be31280d82442e4de81
Raphael Jean <raphael.jean@rocketmail.com>
2020-11-02 14:24:55 -0500

    Merge

--- control/launchers/run_all.sh
+++ control/launchers/run_all.sh
@@ -3,6 +3,7 @@ source /environment.sh
 source /opt/ros/noetic/setup.bash
 source /code/catkin_ws/devel/setup.bash
 source /code/exercise_ws/devel/setup.bash
+pip3 install debugpy
 python3 /code/solution.py &
 roslaunch --wait car_interface all.launch veh:=$VEHICLE_NAME &
 roslaunch --wait duckietown_demos lane_following.launch
