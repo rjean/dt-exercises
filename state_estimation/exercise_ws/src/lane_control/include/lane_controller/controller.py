74f1416039042993dc5021565ab1dd72522f8899
Raphael Jean <raphael.jean@rocketmail.com>
2020-11-23 18:24:29 -0500

    Added missing nodes.

---
+++ state_estimation/exercise_ws/src/lane_control/include/lane_controller/controller.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+
+class PurePursuitLaneController:
+    """
+    The Lane Controller can be used to compute control commands from pose estimations.
+
+    The control commands are in terms of linear and angular velocity (v, omega). The input are errors in the relative
+    pose of the Duckiebot in the current lane.
+
+    """
+
+    def __init__(self, parameters):
+
+        self.parameters = parameters
+
+    def update_parameters(self, parameters):
+        """Updates parameters of LaneController object.
+
+            Args:
+                parameters (:obj:`dict`): dictionary containing the new parameters for LaneController object.
+        """
+        self.parameters = parameters
+
+
